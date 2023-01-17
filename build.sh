cbc661a37c6187eb1ae719f96463a2da0fb9c566
Romain Fillaudeau <r.fillaudeau@gmail.com>
2023-01-17 18:14:03 +0100

    Changed app folder structure, fixed issues with docker

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo docker compose cp frontend:/usr/src/app/node_modules ./frontend
+sudo docker compose cp backend:/var/www/backend/vendor/ ./backend
+
+docker compose exec backend php bin/console do:mi:mi -n
+docker compose exec backend php bin/console league:oauth2-server:create-client --grant-type=password --grant-type=refresh_token app
